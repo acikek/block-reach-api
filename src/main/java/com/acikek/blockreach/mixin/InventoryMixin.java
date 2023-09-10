e3ebf925ef3e8e6dd36344cd8825007ad68bac3c
Skye Prince <spiralixp@gmail.com>
2023-09-10 15:03:34 -0700

    oop

--- src/main/java/com/acikek/blockreach/mixin/InventoryMixin.java
+++ src/main/java/com/acikek/blockreach/mixin/InventoryMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public interface InventoryMixin {
 
     @Inject(method = "canPlayerUse(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/player/PlayerEntity;I)Z", cancellable = true, at = @At("HEAD"))
-    private static void calibrated$fakeUsable(BlockEntity blockEntity, PlayerEntity player, int range, CallbackInfoReturnable<Boolean> cir) {
+    private static void blockreachapi$fakeUsable(BlockEntity blockEntity, PlayerEntity player, int range, CallbackInfoReturnable<Boolean> cir) {
         if (BlockReachAPI.hasBlockEntity(player, blockEntity)) {
             cir.setReturnValue(true);
         }
